0d9fee18b32de3184eb43b06623d310b9056f172
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-21 15:25:47 +0000

    wrote frontend cookie funcs

---
+++ src/common/index.js
@@ -0,0 +1,21 @@
+export const writeCookie = (key, value, days = 365) => {
+    let date = new Date()
+
+    date.setDate(date.getDate()+ days);
+
+    const cookie = (document.cookie =
+        key + "=", value + "; expires" + date.toGMTString() + "; path=/");
+
+        return cookie;
+};
+
+export const getTokenFromCookie = (cookiename) => {
+    const expression = new RegExp (`?<=${cookieName}=)[^;]*`);
+
+    try {
+        const cookie = document.cookie.match(expression)[0]
+    } catch {
+        console.log("cookie not found")
+        return false;
+    }
+};
