444a80ebf23db23c1c56c7a632193497e433c847
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-22 12:47:11 +0000

    add registerUser to utils/user.js and Register to Register.js

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,6 +1,33 @@
-//import {writeCookie} from "../common"
+import { writeCookie } from "../common";
 
-//export registerUser...
+export const registerUser = async (e, username, password) => {
+    console.log(username, password)
+    try {
+        const response = await fetch(
+            `${process.env.REACT_APP_BASE_URL}/users/register`,
+                { //don't understand the error here?
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body:
+                    JSON.stringify({
+                        username: username,
+                        password: password,
+                    }),
+                }
+             );
+
+             const data = await response.json();
+             writeCookie("jwt_token", data.user.token, 14);
+
+             e.target.reset();
+             return data;
+    } catch (error) {
+        console.log(error)
+    }
+    };
 
 export const login = async (e, username, password) => {
     console.log("login function called")
