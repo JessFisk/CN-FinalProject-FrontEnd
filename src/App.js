d8b9a7996679834dc18a236d061604f9a8bf0939
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-22 16:05:31 +0000

    made some notes on things to keep

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import {useState, useEffect} from "react"
 
 import './App.css';
-// import {useState, useEffect} from "react";
 
+// import RegisterOrLogContainer from "./components/registerOrLogContainer/RegisterOrLogContainer";
+// import Homepage from './components/homepage/Homepage';
+
+import { getTokenFromCookie } from './common'; //keep
+import { authCheck } from './utils/user'; //keep
+
+//!!! all of this down to the return might be necessary to keep!!!
 function App() {
-  return (
+    const user = [user, setUser] = useState(null);
+    //***other states will need to be set here***
+
+    console.log("hello world");
+
+    useEffect(() => {
+      if (document.cookie){
+        let token = getTokenFromCookie("jwt_token")
+
+        if (token === "false") {
+          setUser(null);
+        } else {
+          loginWithToken(token)
+        }
+      }
+    }, []);
+
+    const loginWithToken = async (token) => {
+      const persistentUser = await authCheck(token);
+
+      await setUser(persistentUser.user);
+      // await setRecipes(persistentUser.recipes)
+      //await other setX for whatever's displayed
+    }
+
+  return ( //all this can be changed to React-Router stuff
     <div className="App">
       <h1> Hello World</h1>
+      <Homepage
+      user = {user}
+      setUser = {setUser}/>
+      <RegisterOrLogContainer
+      user = {user}
+      setUser ={setUser} />
     </div>
   );
 }
