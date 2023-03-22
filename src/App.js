9364ec9c34356a425e2060bff6c68dc213cd5be6
Jessica Fisk <fisk.jess@gmail.com>
2023-03-22 14:51:11 +0000

    used react router for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CheckoutPage from "./pages/checkoutPage/CheckoutPage"
+import LandingPage from "./pages/landingPage/LandingPage"
+import ContactPage from "./pages/contactPage/ContactPage"
 // import {useState, useEffect} from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1> Hello World</h1>
-    </div>
+    <BrowserRouter>
+    
+  
+  <Routes>
+    <Route path="/" element={<LandingPage/>}></Route>
+    <Route path="/checkout" element={<CheckoutPage/>}></Route>
+    <Route path="/contact" element={<ContactPage/>}></Route>
+  </Routes>
+
+</BrowserRouter>
   );
 }
 
---
+++ src/pages/landingPage/LandingPage.js
@@ -0,0 +1,20 @@
+
+
+
+const LandingPage = (props) => {
+
+    return (
+        <div>
+            <h1> NOM... </h1>
+        </div>
+    )
+}
+
+
+
+
+
+
+export default LandingPage;
+
+
