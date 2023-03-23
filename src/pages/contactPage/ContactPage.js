dc1af8a1533d11c2063dea57f5ab2288e7a9cc20
CatherineStone <106274539+CatherineStone@users.noreply.github.com>
2023-03-23 11:53:14 +0000

    contact page

--- src/pages/contactPage/ContactPage.js
+++ src/pages/contactPage/ContactPage.js
@@ -1,13 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./ContactPage.css"
+
 
 const ContactPage = (props) => {
-
     return (
-        <div>
-            <h1> Contact Us </h1>
+        <form id="contact-form">
+        <p id="contact-us">Contact Us</p>
+        <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" />
         </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" aria-describedby="emailHelp" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea className="form-control" rows="5"></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
     )
-}
+};
 
 
 
