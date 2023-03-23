e55f5914a86bf93268b2d0bc82361e6d66a8e01e
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-23 11:26:56 +0000

    work on LandingPage.css. Deleted Homepage as redundant

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -3,14 +3,14 @@ import RegisterOrLogContainer from '../../components/registerOrLogContainer/Regi
 import {useState} from 'react'
 import Logo3 from "../../components/images/Logo3.png"
 
-import "./LandingPage.css";
+import "../landingPage/LandingPage.css";
 
 const LandingPage = (props) => {
     const [user, setUser] = useState();
     return (
         <div className="LandingPage-Wrapper">
-            <h1> NOM... </h1>
-            <h3> Delicous Food for Every Mood</h3>
+            <h1 id='Title'> NOM... </h1>
+            <h3 id='slogan'> Delicous Food for Every Mood</h3>
             <img id="logo" src={Logo3} alt="nom green logo"></img>
 
             <RegisterOrLogContainer user={user} setUser={setUser}/>
