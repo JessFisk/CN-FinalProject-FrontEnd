b3b82bd477712ff33e2c74ae60a17cbe63187357
Jessica Fisk <fisk.jess@gmail.com>
2023-03-26 15:00:10 +0100

    login and persistent user working and displayed on screen, some Css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import {
     Nav,
     Navbar,
     Dropdown,
+    Button,
 } from "react-bootstrap"
 import { FaShoppingCart } from "react-icons/fa"
 import { Link } from "react-router-dom";
@@ -11,10 +12,11 @@ import "./Header.css";
 import Logo from "../images/WhiteNom.png"
 
 const Header = (props) => {
+    console.log(props)
     return <Navbar id="navbarBackground" variant="dark" style={{ height: 80 }}>
         <Container className="navBar">
             <Navbar.Brand className="routesContainer">
-                <a href="/"><img id="Logo" className="links" src={Logo} alt="logo"/></a>
+                <a href="/"><img id="Logo" className="links" src={Logo} alt="logo" /></a>
                 <Link id="recipes" className="links" to="/meals">Recipes</Link>
                 <Link id="profile" className="links" to="/profile">Profile</Link>
                 <Link id="about" className="links" to="/about">About</Link>
@@ -27,34 +29,52 @@ const Header = (props) => {
                         <FaShoppingCart color="white" fontSize="25px" />
                         <Badge bg="none">{props.basket.length}</Badge>
                     </Dropdown.Toggle>
-                    <button>
-                        <a href="/login">Login</a>
-                    </button>
+                    <Button href="/login">
+                        Login
+                    </Button>
                     <Dropdown.Menu style={{ minWidth: 370 }}>
-                    {props.basket.map((recipe, index)=>{
-                        return(
-                            <Dropdown.Item>
-                            <div key={index} className="checkoutRecipeBoxes">
+                        {props.basket.map((recipe, index) => {
+                            return (
+                                <Dropdown.Item>
+                                    <div key={index} className="checkoutRecipeBoxes">
 
-                            <div className="basketItemNames">
-                            <p>{recipe.name}</p>
-                            </div>
-              
-                            <div className="basketItemPrice">
-                            <p>{recipe.price}</p>
-                            </div>
-              
-                            <button className="basketRemoveButton">
-                            &times;
-                            </button>
-                            </div>
-                            </Dropdown.Item>)})}
-                            <Link className="goToCheckoutButtons" to={"/Checkout"}>Go to check out</Link>
+                                        <div className="basketItemNames">
+                                            <p>{recipe.name}</p>
+                                        </div>
+
+                                        <div className="basketItemPrice">
+                                            <p>{recipe.price}</p>
+                                        </div>
+
+                                        <button className="basketRemoveButton">
+                                            &times;
+                                        </button>
+                                    </div>
+                                </Dropdown.Item>)
+                        })}
+                        <Link className="goToCheckoutButtons" to={"/Checkout"}>Go to check out</Link>
                     </Dropdown.Menu>
                 </Dropdown>
+
+                {/* Creating a true/false boolean- meaning if its true this element will be rendered if its false it will do nothing.
+                so this is like an if statement using && */}
+                {
+                    props.user && (
+                        <>
+                            <Navbar.Text>
+                                Welcome: {props.user.username}
+                            </Navbar.Text>
+
+                            <Button href="/login">
+                                Logout
+                            </Button>
+                        </>
+                    )
+                }
+
             </Nav>
         </Container>
-        
+
     </Navbar>
 };
 
