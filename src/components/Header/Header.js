74ff4f886bcb48e46b49e679f5e04010780dbb05
Jessica Fisk <fisk.jess@gmail.com>
2023-03-29 10:09:36 +0100

    Nav Bar update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,86 +16,89 @@ import { useNavigate } from "react-router-dom";
 const Header = (props) => {
     console.log(props)
     const navigate = useNavigate()
-    return <Navbar id="navbarBackground" variant="dark" style={{ height: 80 }}>
-        <Container className="navBar">
-            <Navbar.Brand className="routesContainer">
-                <a href="/"><img id="Logo" className="links" src={Logo} alt="logo" /></a>
-                <Link id="recipes" className="links" to="/meals">Recipes</Link>
-                <Link id="profile" className="links" to="/profile">Profile</Link>
-                <Link id="about" className="links" to="/about">About</Link>
-                <Link id="contact" className="links" to="/contact">Contact</Link>
-                <Link id="community" className="links" to="/community">Community</Link>
-            </Navbar.Brand>
-            <Nav>
-                <Dropdown id="cart">
-                    <Dropdown.Toggle variant="success">
-                        <FaShoppingCart color="white" fontSize="25px" />
-                        <Badge bg="none">{props.basket.length}</Badge>
-                    </Dropdown.Toggle>
-                    {/* This means the login button will hide when someone is logged in.
-                    (only showing the Welcome:username and logout) */}
-                    {
-                        !props.user && (
-                            <Button href="/login"
-                            id="loginBtn">
-                                Login
-                            </Button>
-                        )
-                    }
+    return (
+        <Navbar collapseOnSelect expand="lg" id="navbarBackground">
+            <Container>
+                <Navbar.Brand href="/"><img id="Logo" src={Logo} alt="logo" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link className="nav-link links" to="/meals">Recipes</Link>
+                        <Link className="nav-link links" to="/profile">Profile</Link>
+                        <Link className="nav-link links" to="/about">About</Link>
+                        <Link className="nav-link links" to="/contact">Contact</Link>
+                        <Link className="nav-link links" to="/community">Community</Link>
+                    </Nav>
+                    <Nav>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success">
+                                <FaShoppingCart color="white" fontSize="25px" />
+                                <Badge bg="none">{props.basket.length}</Badge>
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu style={{ minWidth: 370 }}>
-                        {props.basket.map((recipe, index) => {
-                            return (
-                                <Dropdown.Item>
-                                    <div key={index} className="checkoutRecipeBoxes">
+                            <Dropdown.Menu style={{ minWidth: 370 }}>
+                                {props.basket.map((recipe, index) => {
+                                    return (
+                                        <Dropdown.Item>
+                                            <div key={index} className="checkoutRecipeBoxes">
 
-                                        <div className="basketItemNames">
-                                            <p>{recipe.title}</p>
-                                        </div>
+                                                <div className="basketItemNames">
+                                                    <p>{recipe.title}</p>
+                                                </div>
 
-                                        <div className="basketItemPrice">
-                                            <p>£{recipe.pricePerServing.toFixed(2/10)/10}</p>
-                                        </div>
+                                                <div className="basketItemPrice">
+                                                    <p>£{recipe.pricePerServing.toFixed(2 / 10) / 10}</p>
+                                                </div>
 
-                                        <button className="basketRemoveButton">
-                                            &times;
-                                        </button>
-                                    </div>
-                                </Dropdown.Item>)
-                        })}
-                        <Link className="goToCheckoutButtons" to={"/checkout"}>Go to check out</Link>
-                    </Dropdown.Menu>
-                </Dropdown>
+                                                <button className="basketRemoveButton">
+                                                    &times;
+                                                </button>
+                                            </div>
+                                        </Dropdown.Item>)
+                                })}
+                                <Link className="goToCheckoutButtons" to={"/checkout"}>Go to check out</Link>
+                            </Dropdown.Menu>
+                        </Dropdown>
 
-                {/* Creating a true/false boolean- meaning if its true this element will be rendered if its false it will do nothing.
+                        {/* This means the login button will hide when someone is logged in.
+                     (only showing the Welcome:username and logout) */}
+                        {
+                            !props.user && (
+                                <Button href="/login"
+                                    id="loginBtn">
+                                    Login
+                                </Button>
+                            )
+                        }
+                        {/* Creating a true/false boolean- meaning if its true this element will be rendered if its false it will do nothing.
                 so this is like an if statement using && */}
-                {
-                    props.user && (
-                        <>
-                            <Navbar.Text className="welcomeText">
-                                Welcome, {props.user.username}!
-                            </Navbar.Text>
+                        {
+                            props.user && (
+                                <>
+                                    <Navbar.Text className="welcomeText">
+                                        Welcome, {props.user.username}!
+                                    </Navbar.Text>
 
-                            {/* Added Logout button with onClick event that also navigates 
+                                    {/* Added Logout button with onClick event that also navigates 
                             back to the landing page on logout need to add use navigate at top too.*/}
-                            <Button
-                            id="logoutBtn"
-                                onClick={
-                                    () => {
-                                        logout(props.setUser);
-                                        navigate("/")
-                                    }
-                                }>
-                                Logout
-                            </Button>
-                        </>
-                    )
-                }
-
-            </Nav>
-        </Container>
-
-    </Navbar>
+                                    <Button
+                                        id="logoutBtn"
+                                        onClick={
+                                            () => {
+                                                logout(props.setUser);
+                                                navigate("/")
+                                            }
+                                        }>
+                                        Logout
+                                    </Button>
+                                </>
+                            )
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 };
 
 export default Header;
